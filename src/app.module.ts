25be2bc7413282bcaeeda8187c5a181bd6cb9e4e
rohollah <rohollah.ayoobi.rh@gmail.com>
2025-05-03 18:49:38 +0330

    feat(positions): Implement full CRUD operations for positions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FileUploadModule } from './file-upload/file-upload.module';
 import { MembersModule } from './members/members.module';
+import { PositionsModule } from './positions/positions.module';
 
 @Module({
-  imports: [UsersModule, FileUploadModule, MembersModule],
+  imports: [UsersModule, FileUploadModule, MembersModule, PositionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/positions/positions.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreatePositionDto } from './dto/create-position.dto';
+import { PositionsService } from './positions.service';
+import { UpdatePositionDto } from './dto/update-position.dto';
+
+@Controller('positions')
+export class PositionsController {
+  constructor(private readonly positionService: PositionsService) {}
+
+  @Post()
+  async create(@Body() createPositionDto: CreatePositionDto) {
+    return this.positionService.create(createPositionDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.positionService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.positionService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updatePositionDto: UpdatePositionDto,
+  ) {
+    return this.positionService.update(id, updatePositionDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.positionService.remove(id);
+  }
+}
