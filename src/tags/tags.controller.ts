b2d61fb6883c3757c74e00cc09490c842cf46787
rohollah <rohollah.ayoobi.rh@gmail.com>
2025-06-15 12:21:06 +0330

    feat(tags): secured tags routes with JWT auth guard

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -6,36 +6,43 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TagsService } from './tags.service';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagService: TagsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createTagDto: CreateTagDto) {
     return this.tagService.create(createTagDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll() {
     return this.tagService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: number) {
     return this.tagService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async update(@Param('id') id: number, @Body() updateTagDto: UpdateTagDto) {
     return this.tagService.update(id, updateTagDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: number) {
     return this.tagService.remove(id);
