cf15875fb15927442dcdefb25d5a88ce3d0b07a0
rohollah <rohollah.ayoobi.rh@gmail.com>
2025-06-16 14:11:45 +0330

    feat(auth): implement JWT authentication with cookie support
    - Implemented JWT authentication for backend routes using Passport strategy.
    - Added cookie-based token handling for secure user login and authentication.
    - Protected admin routes to restrict access to authenticated users only.

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,13 +1,20 @@
 // src/auth/jwt.strategy.ts
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req) => {
+        // بخونه از کوکی به جای هدر
+        if (req?.cookies?.token) {
+          return req.cookies.token;
+        }
+        return null;
+      },
+      ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET || 'SECRET',
     });
   }
