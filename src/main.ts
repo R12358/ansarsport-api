931672fbeb76c8064a3b59e8c8f3d0d1c452b2c2
rohollah <rohollah.ayoobi.rh@gmail.com>
2025-07-12 17:58:48 +0330

    chore: update CORS origin to use HTTPS for production frontend

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://ansarsport.ir', // جایی که فرانت اجرا میشه
+    origin: 'https://ansarsport.ir', // جایی که فرانت اجرا میشه
     credentials: true, // اگر از کوکی یا هدر خاص استفاده می‌کنی
   });
 
